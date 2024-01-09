2f8d9b7e781fac06670fde03b8ab0b3d2d9ec3fb
ImGMin <76603110+ImGMin@users.noreply.github.com>
2024-01-09 19:35:34 +0900

    0109
    추가 버튼
    일정 추가 창
    일정 이름 입력칸

--- app/src/main/java/com/example/timemaker/MainActivity.java
+++ app/src/main/java/com/example/timemaker/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.timemaker;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button addToDo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addToDo = findViewById(R.id.addToDo);
+        addToDo.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void correct(View view){
+        Toast.makeText(this, "Yes",Toast.LENGTH_LONG).show();
     }
 }
