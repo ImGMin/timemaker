78f97642bc5f94c70a3d4bdae007e6ad2cfe3ab4
ImGMin <76603110+ImGMin@users.noreply.github.com>
2024-01-24 17:39:22 +0900

    01.24
    스위치 연결, 입력창 껐다켰다

--- app/src/main/java/com/example/timemaker/AddTodo.java
+++ app/src/main/java/com/example/timemaker/AddTodo.java
@@ -4,15 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 public class AddTodo extends AppCompatActivity {
     Button addToDocomplete_B;
     Button addToDodelete_B;
     Button addToDoreturn_B;
+    ConstraintLayout layoutdoing;
+    EditText addToDodoing;
+    ConstraintLayout layoutss;
+    EditText addToDostart;
+    EditText addToDostop;
     Switch addToDoswitch;
     ImageView image1;
     ImageView image2;
@@ -22,17 +31,47 @@ public class AddTodo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addtodo);
 
-        image1 = findViewById(R.id.imageView2);
-        image2 = findViewById(R.id.imageView3);
-
+        layoutdoing = findViewById(R.id.layouttimedoing);
+        layoutss = findViewById(R.id.layouttimess);
+        addToDodoing = findViewById(R.id.addToDo_doingtime);
+        addToDostart = findViewById(R.id.addToDo_starttime);
+        addToDostop = findViewById(R.id.addToDo_finishtime);
         addToDoswitch = findViewById(R.id.addToDoswitch);
-        CheckState();
-        addToDoswitch.setOnClickListener(new View.OnClickListener() {
+        //CheckState();
+
+        addToDoswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                CheckState();
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(!addToDoswitch.isChecked()){
+                    addToDostart.setFocusable(true);//포커싱과
+                    addToDostop.setFocusable(true);//포커싱과
+                    addToDostart.setClickable(true);//터치가 안되도록!
+                    addToDostop.setClickable(true);//터치가 안되도록!
+
+                    addToDodoing.setFocusable(false);//포커싱과
+                    addToDodoing.setClickable(false);//터치가 안되도록!
+                }
+                else {
+                    //image1.onVisibilityAggregated(true);
+                    //image2.onVisibilityAggregated(false);
+                    //layoutss.setBackgroundColor(ContextCompat.getColor(this,R.color.Gray));//배경색 설정
+                    addToDodoing.setFocusable(true);//포커싱과
+                    addToDodoing.setClickable(true);//터치가 안되도록!
+
+                    addToDostart.setFocusable(false);//포커싱과
+                    addToDostop.setFocusable(false);//포커싱과
+                    addToDostart.setClickable(false);//터치가 안되도록!
+                    addToDostop.setClickable(false);//터치가 안되도록!
+                }
             }
-        });
+        }) ;
+        //{
+        ////    @Override
+////
+        ////    public void onCheckedChange(){
+        ////        CheckState();
+        ////    }
+        ////});
 
 
 
@@ -65,9 +104,12 @@ public class AddTodo extends AppCompatActivity {
     }
 
     private void CheckState() {
-        if(addToDoswitch.isChecked()){
+        if(!addToDoswitch.isChecked()){
             image1.onVisibilityAggregated(false);
             image2.onVisibilityAggregated(true);
+            layoutdoing.setBackgroundColor(ContextCompat.getColor(this,R.color.Gray));//배경색 설정
+            addToDodoing.setFocusable(false);//포커싱과
+            addToDodoing.setClickable(false);//터치가 안되도록!
         }
         else {
             image1.onVisibilityAggregated(true);
